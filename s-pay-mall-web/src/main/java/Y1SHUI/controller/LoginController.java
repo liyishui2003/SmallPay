7494c4c4f2f95048db2a9b7ebe640ab44d33aa56
liyishui <3324598368@qq.com>
2025-04-03 16:48:53 +0800

    测试 微信用户登录 通过

--- s-pay-mall-web/src/main/java/Y1SHUI/controller/LoginController.java
+++ s-pay-mall-web/src/main/java/Y1SHUI/controller/LoginController.java
@@ -4,10 +4,8 @@ import Y1SHUI.common.constants.Constants;
 import Y1SHUI.common.response.Response;
 import Y1SHUI.service.ILoginService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -22,18 +20,47 @@ public class LoginController {
     private ILoginService loginService;
 
     @RequestMapping(value = "weixin_qrcode_ticket",method = RequestMethod.GET)
-
     public Response<String> weixinQrCodeTicket(){
         try {
             String qrCodeTicket = loginService.createQrCodeTicket();
-            log.info(("生成微信扫码登录 ticket:{}",qrCodeTicket);
+            log.info("生成微信扫码登录 ticket:{}",qrCodeTicket);
             return Response.<String>builder()
                     .code(Constants.ResponseCode.SUCCESS.getCode())
                     .info(Constants.ResponseCode.SUCCESS.getInfo())
                     .data(qrCodeTicket)
                     .build();
         } catch (Exception e){
+            log.error("生成微信扫码登录 ticket 失败",e);
+            return Response.<String>builder()
+                    .code(Constants.ResponseCode.UN_ERROR.getCode())
+                    .info(Constants.ResponseCode.UN_ERROR.getCode())
+                    .build();
+        }
+    }
 
+    @RequestMapping(value = "check_login",method =  RequestMethod.GET)
+    public  Response<String> checkLogin(@RequestParam String ticket){
+        try {
+            String openidToken = loginService.checkLogin(ticket);
+            log.info("扫码检测登录结果 ticket:{} openidToken:{}",ticket,openidToken);
+            if(StringUtils.isNotBlank(openidToken)) {
+                return Response.<String>builder()
+                        .code(Constants.ResponseCode.SUCCESS.getCode())
+                        .info(Constants.ResponseCode.SUCCESS.getInfo())
+                        .data(openidToken)
+                        .build();
+            } else {
+                return Response.<String>builder()
+                        .code(Constants.ResponseCode.NO_LOGIN.getCode())
+                        .info(Constants.ResponseCode.NO_LOGIN.getCode())
+                        .build();
+            }
+        } catch (Exception e){
+            log.error("扫码检测登录结果失败 ticket:{}",ticket,e);
+            return Response.<String>builder()
+                    .code(Constants.ResponseCode.UN_ERROR.getCode())
+                    .info(Constants.ResponseCode.UN_ERROR.getInfo())
+                    .build();
         }
     }
 
